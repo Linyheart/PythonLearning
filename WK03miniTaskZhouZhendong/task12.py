8ae419772dd5e0b055789ac79e024173f6694a22
Linyheart <785989657@qq.com>
2019-09-19 21:07:24 +0800

    任务12

--- WK03miniTaskZhouZhendong/task12.py
+++ WK03miniTaskZhouZhendong/task12.py
@@ -1,6 +1,13 @@
 # 12.用str.format()输出一个三行 (含表头)三列表格
-from prettytable import PrettyTable
-tb = pt.PrettyTable()
-
-
+print('{:^{}}'.format("姓名", 10), end="\t")
+print('{:^{}}'.format("学科", 10), end="\t")
+print('{:^{}}'.format("成绩", 10))
+print('{:<{}}'.format("张三", 10), end="\t")
+print('{:<{}}'.format("数学", 10), end="\t")
+print('{:>{}}'.format(90.5, 10))
+print('{:<{}}'.format("李四", 10), end="\t")
+print('{:<{}}'.format("语文（阅读）", 10), end="\t")
+print('{:>{}}'.format(8.0, 10))
+# :^ ：居中；:< ：左对齐；:> ：右对齐
+# \t 制表符
 
