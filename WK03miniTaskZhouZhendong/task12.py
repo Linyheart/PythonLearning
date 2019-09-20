e0894a6d0ffd8fcd2733c05592f2276d2b9c75e1
Linyheart <linyheart@linyheart.com>
2019-09-20 10:38:07 +0800

    课上操作

--- WK03miniTaskZhouZhendong/task12.py
+++ WK03miniTaskZhouZhendong/task12.py
@@ -1,11 +1,11 @@
 # 12.用str.format()输出一个三行 (含表头)三列表格
-print('{:^{}}'.format("姓名", 10), end="\t")
-print('{:^{}}'.format("学科", 10), end="\t")
+print('{:^{}}'.format("姓名", 10), end="\t\t")
+print('{:^{}}'.format("学科", 10), end="\t\t")
 print('{:^{}}'.format("成绩", 10))
-print('{:<{}}'.format("张三", 10), end="\t")
-print('{:<{}}'.format("数学", 10), end="\t")
+print('{:<{}}'.format("张三", 10), end="\t\t")
+print('{:<{}}'.format("数学", 10), end="\t\t")
 print('{:>{}}'.format(90.5, 10))
-print('{:<{}}'.format("李四", 10), end="\t")
+print('{:<{}}'.format("李四", 10), end="\t\t")
 print('{:<{}}'.format("语文（阅读）", 10), end="\t")
 print('{:>{}}'.format(8.0, 10))
 # :^ ：居中；:< ：左对齐；:> ：右对齐
