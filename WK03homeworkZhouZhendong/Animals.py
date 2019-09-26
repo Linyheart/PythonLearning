1f2d63eedb74d83938679394da39f7211856b2df
Linyheart <785989657@qq.com>
2019-09-26 23:07:19 +0800

    wk3homework

---
+++ WK03homeworkZhouZhendong/Animals.py
@@ -0,0 +1,6 @@
+"""
+使用Pyhton面向对象为Dogs进行建模
+这里的Dogs包括Shiba, Husky, Corgi三种犬类，分别定义成三个类
+所有的Dogs都有name, color,
+不同的犬类有自己的 feature
+"""
--- WK03homeworkZhouZhendong/WK03-01.py
+++ WK03homeworkZhouZhendong/WK03-01.py
@@ -1,8 +1,24 @@
 import win32ui
+import numpy
 dlg = win32ui.CreateFileDialog(1)
 dlg.DoModal()
-
-filepath = dlg.GetPath()
-filename = dlg.GetPathName()
-print(filepath)
+filepath = dlg.GetPathName()
+file = open(filepath, encoding="utf-8")
+dataLine = file.readlines()
+file.close()
+for i in range(0, len(dataLine)):
+    if dataLine[i][0] == '#':
+        dataLine[i] = 'deleteThis'
+    dataLine[i] = dataLine[i].replace("\n", '')
+    if dataLine[i] == '':
+        dataLine[i] = 'deleteThis'
+dataLine = filter(lambda x: x != 'deleteThis', dataLine)
+newDateLine = list(dataLine)
+data = list(map(int, newDateLine))
+print("数字个数："+str(len(data)))
+print("最大值："+str(max(data)))
+print("最小值："+str(min(data)))
+print("和："+str(sum(data)))
+print("平均值："+str(round(numpy.mean(data), 2))+"（保留两位小数）")
+print("标准差："+str(round(numpy.std(data), 2))+"（保留两位小数）")
 
