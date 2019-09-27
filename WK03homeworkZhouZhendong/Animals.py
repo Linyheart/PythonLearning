e3273791f0d97211ba541be1c08090f53934a3ca
Linyheart <linyheart@linyheart.com>
2019-09-27 16:21:37 +0800

    wk03homework

--- WK03homeworkZhouZhendong/Animals.py
+++ WK03homeworkZhouZhendong/Animals.py
@@ -1,15 +1,40 @@
 """
-使用Pyhton面向对象为Animals进行建模
+使用Pyhton面向对象为Pets进行建模
 这里的Pets主要包括Cat, Dog两类，分别定义成两个类
 所有的Pets都有name, color两个属性，所有的Pets都可以takeFood, sleep
-Cat有
+Cat有type, pelage两个属性，所有的Cat都可以meow, scratch
+Dog有type, size两个属性，所有的Dog都可以bark, bite
 """
-class Dogs(object):
+import time
+
+
+class Pets(object):
+
     def __init__(self, name, color):
         self.name = name
         self.color = color
 
-class Shiba(Dogs):
+    def takefood(self):
+        print("您的宠物开始进食，请不要打扰它~")
+        time.sleep(1)
+        print("您的宠物吃得正开心~")
+        time.sleep(1)
+        print("您的宠物快吃完啦！")
+        time.sleep(1)
+        print("您的宠物吃完啦！吃饱饱，有精神！")
+
+    def sleep(self):
+        print("您的宠物有点困了，要睡觉啦！")
+        time.sleep(1)
+        print("您的宠物睡得正香~")
+        time.sleep(1)
+        print("您的宠物还在梦里哟~")
+        time.sleep(1)
+        print("您的宠物睡醒啦！快抱抱它吧!")
+
+
+
+class Cat(Pets):
     def __init__(self, feature, *args, **kwargs):
         self.feature = feature
         super(Dogs, self).__init__(*args, **kwargs)
