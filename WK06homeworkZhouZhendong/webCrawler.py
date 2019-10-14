599ad982552314244796c543f8aa1b855e57c2f0
Linyheart <785989657@qq.com>
2019-10-14 16:28:19 +0800

    wk06homework

--- WK06homeworkZhouZhendong/webCrawler.py
+++ WK06homeworkZhouZhendong/webCrawler.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-urlHtml = "http://xteaching.com/book2019/list.php"
-urlJson = "http://xteaching.com/book2019/listjson.php"
-urlAddBook = "http://xteaching.com/book2019/add.php"
+
+def main():
+    url_html = "http://xteaching.com/book2019/list.php"
+    url_json = "http://xteaching.com/book2019/listjson.php"
+    url_add_book = "http://xteaching.com/book2019/add.php"
+
+    add_book(url_add_book, "三体Ⅱ·黑暗森林", "刘慈欣", "2008")
+    get_from_html(url_html)
+    get_from_json(url_json)
 
 
 def get_from_html(url):
     page_html = requests.get(url)
+    page_html.encoding = 'utf-8'
     soup = BeautifulSoup(page_html.text, 'lxml')
     xml = soup.find_all('tr', class_="")
     book_info = []
@@ -18,7 +26,6 @@ def get_from_html(url):
     for i in range(0, len(book_info)):
         if book_info[i] not in book_info_unique:
             book_info_unique.append(book_info[i])
-
     log_lines = []
     for i in range(0, len(book_info_unique)):
         for j in range(0, len(book_info_unique[i])):
@@ -27,8 +34,39 @@ def get_from_html(url):
             else:
                 log_lines.append(book_info_unique[i][j].string)
         log_lines.append("\n")
-    print(log_lines)
     log = open('bookInfoFromHtml.txt', 'w')
     log.writelines(log_lines)
 
 
+def get_from_json(url):
+    page_json = requests.get(url)
+    page_json.encoding = 'utf-8'
+    book_info = json.loads(page_json.text)
+
+    book_info_unique = []
+    for i in range(0, len(book_info)):
+        if book_info[i] not in book_info_unique:
+            book_info_unique.append(book_info[i])
+
+    log_lines = []
+    for i in range(0, len(book_info_unique)):
+        for j in range(0, len(book_info_unique[i])):
+            if j != len(book_info_unique[i]) - 1:
+                log_lines.append(book_info_unique[i][j] + ",")
+            else:
+                log_lines.append(book_info_unique[i][j])
+        log_lines.append("\n")
+
+    log = open('bookInfoFromJson.txt', 'w')
+    log.writelines(log_lines)
+
+
+def add_book(url, name, author, year):
+    response = requests.post(url, {"ti": name, "au": author, "y": year, "code": "2019pynet"})
+    if response.status_code == 200:
+        print(name + ", " + author + ", " + year + ", " + "添加成功！")
+    else:
+        print(name + ", " + author + ", " + year + ", " + "添加失败！请重试！")
+
+
+main()
