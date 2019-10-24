9992f267acf0e0a7791e9c06e8a4c94ad776911c
Linyheart <785989657@qq.com>
2019-10-24 21:58:56 +0800

    fixWK07Homework

--- WK07homeworkZhouZhendong/pictureCrawler.py
+++ WK07homeworkZhouZhendong/pictureCrawler.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 picurl = "http://xteaching.com/book2019/images/"
 
@@ -14,11 +15,17 @@ def download_picture(url):
     for xml_label in xml:
         if re.match('^(BingWallpaper).*(\.jpg)$', xml_label['href']):
             pictures_label.append(xml_label)
+    folder_path = "c:\\BingWP\\"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
     for picture in pictures_label:
         request_picture = requests.get(url + picture['href'])
         if request_picture.status_code == 200:
             if int(request_picture.headers['Content-Length'])/1024 < 500:
                 with open(picture['href'], 'wb') as picture_file:
+                    picture_file.write(request_picture.content)
+                picture_path = folder_path + picture['href']
+                with open(picture_path, 'wb') as picture_file:
                     picture_file.write(request_picture.content)
 
 
