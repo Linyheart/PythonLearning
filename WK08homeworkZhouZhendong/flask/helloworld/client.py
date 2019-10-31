09727a08fb8b1b540de9dcf7ab66008d85773899
Linyheart <785989657@qq.com>
2019-10-31 22:36:53 +0800

    WK08homeworkFinished

---
+++ WK08homeworkZhouZhendong/flask/helloworld/client.py
@@ -0,0 +1,9 @@
+import requests
+
+file_data = {'image': open('flask.jpg', 'rb')}
+
+user_info = {'info': 'flask'}
+
+r = requests.post("http://127.0.0.1:5000/upload", data=user_info, files=file_data)
+
+print(r.text)
