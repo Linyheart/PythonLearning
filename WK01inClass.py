780831b8792a9c3e2d913c039bfe831073ab3c12
Linyheart <785989657@qq.com>
2019-11-17 20:42:09 +0800

    sync

--- WK01inClass.py
+++ WK01inClass.py
@@ -1,29 +1,7 @@
-# for i in range(100):
-#     print('this is line {}'.format(i))
 
-# def f1():
-#     print('...')
-#     return 1
-# # f1()
-# # print(f1())
-# # print(f1)
-# f2=f1
-# f2()
-# f2=f1()
-# f2()
 
-# def f2(a,b):
-#     return a+b
-# print (f2(1.1,3))
-# print(f2('ab','c'))
-# print(f2('abc',123))
+def mixly_mapping(v, al, ah, bl, bh):
+    return bl + (bh - bl) * (v - al) / (ah - al)
 
-d1 = {'a': 'apple', 'b': 'boy', 'c': 'cat', 'd': 'dog'}
-d1
-d1.items()
-d1.keys()
-d1.values()
-d1['a']
-d1['e'] = 'egg'
-for k, v in d1.items():
-    print(k, "=", v)
+
+print(mixly_mapping(60, 50, 80, 56, 74))
