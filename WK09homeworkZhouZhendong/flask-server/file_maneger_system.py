34b684bfc0dae627b18188af5c8ebeb98c8bb1fe
Linyheart <785989657@qq.com>
2019-11-19 10:49:51 +0800

    wk09homework

--- WK09homeworkZhouZhendong/flask-server/file_maneger_system.py
+++ WK09homeworkZhouZhendong/flask-server/file_maneger_system.py
@@ -1,27 +1,33 @@
+import datetime
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+
 
 app = Flask(__name__)
-user_data = open('static/user_data.json', 'a+')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if session['logged_in']:
+        route = 'index.email'
+    else:
+        route = 'login.html'
+    return render_template(route)
 
 
 @app.route('/index.html', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         username = request.form.get('username')
-        user_password = request.form.get('user_password')
-    return render_template('index.html', username=username, user_password=user_password)
+        password = request.form.get('password')
+        session['logged_in'] = True
+    return render_template('index.html', username=username, user_password=password)
 
 
 @app.route('/signup.html', methods=['GET', 'POST'])
 def signup():
-
-    return render_template('signup.html', data=data)
+    user_data = open("static/user_data.json", "a+")
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
