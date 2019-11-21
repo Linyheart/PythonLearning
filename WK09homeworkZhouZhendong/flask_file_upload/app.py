8187ef8b8366613e03897dd3d70883495a2d2d72
Linyheart <785989657@qq.com>
2019-11-21 21:06:30 +0800

    wk09homework

--- WK09homeworkZhouZhendong/flask_file_upload/app.py
+++ WK09homeworkZhouZhendong/flask_file_upload/app.py
@@ -10,24 +10,20 @@ from flask import Flask, url_for, redirect, render_template, request, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(config)  # 导入config里的内容
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)    # 初始化连接数据库
 
 
-class Users(db.Model):
+class Users(db.Model):  # 用户模型
     __tablename__ = 'users_info'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     register_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
-    #
-    # def __init__(self, *args, **kwargs):
-    #     self.username = kwargs.get('username')
-    #     self.password = generate_password_hash(kwargs.get('password'))
 
 
-class Files(db.Model):
+class Files(db.Model):  # 文件模型
     __tablename__ = 'files_info'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String(100), nullable=False)
@@ -38,63 +34,18 @@ class Files(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users_info.id'), nullable=False)
     user = db.relationship('Users', backref=db.backref('Files', order_by=upload_time.desc()))
 
-    def name_replace_blank(self):
-        self.filename = self.filename.replace(' ', '__blank__')
-        return self.filename
 
-    def name_to_blank(self):
-        self.filename = self.filename.replace('__blank__', ' ')
-        return self.filename
-
-
-class Mime_types(db.Model):
+class Mime_types(db.Model): # MIME类型的模型
     __tablename__ = 'mime_type'
     name = db.Column(db.String(100), primary_key=True, nullable=False)
     template = db.Column(db.String(100), nullable=False)
     reference = db.Column(db.String(100), nullable=False)
 
 
-db.create_all()
+db.create_all() # 初始化所有模型，每一个模型都对应数据库的一个的表格
 
 
-# def validate(username, password1, password2=None):
-#     user = Users.query.filter(Users.username == username).first()
-#     if password2:
-#         if len(username) <= 0:
-#             return '请输入用户名'
-#         elif len(password1) <= 0:
-#             return '请输入密码'
-#         else:
-#             if user:
-#                 return '用户名已经存在'
-#             else:
-#                 if len(username) < 4:
-#                     return '用户名长度至少4个字符'
-#                 elif password1 != password2:
-#                     return '两次密码不一致'
-#                 elif len(password1) < 6:
-#                     return '密码长度至少6个字符'
-#                 else:
-#                     return '注册成功'
-#     else:
-#         if len(username) <= 0:
-#             return '请输入用户名'
-#         elif len(password1) <= 0:
-#             return '请输入密码'
-#         else:
-#             if user:
-#                 if check_password_hash(user.password, password1):
-#                     return '登录成功'
-#                 else:
-#                     return '密码错误'
-#             else:
-#                 if len(password2) <= 0:
-#                     return '请再次输入密码'
-#                 else:
-#                     return '用户不存在'
-
-
-def validate(username, password1, password2=None):
+def validate(username, password1, password2=None):  # 注册和登录时检查用户名和密码的合法性
     user = Users.query.filter(Users.username == username).first()
     if password2:
         if user:
@@ -110,8 +61,7 @@ def validate(username, password1, password2=None):
                 return '注册成功，请登录'
     else:
         if user:
-            if check_password_hash(user.password, password1):
-                # if user.password == password1:
+            if check_password_hash(user.password, password1):   # 用hash值检验密码正确与否
                 return '登录成功'
             else:
                 return '密码错误'
@@ -119,17 +69,15 @@ def validate(username, password1, password2=None):
             return '用户不存在'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])    # 首页的视图
 def home():
-    # mime_type = Mime_types.query.filter(Mime_types.name == 'zip').first()
-    # print(mime_type.template)
-    if not session.permanent:
-        return redirect(url_for('login'))
+    if not session.permanent:   # 检查登录状态
+        return redirect(url_for('login'))   # 未登录则跳转到登录界面
     else:
-        return render_template('home.html', username=session['username'])
+        return render_template('home.html', username=session['username'])   # 已登录则进入首页
 
 
-@app.route('/register/', methods=['GET', 'POST'])
+@app.route('/register/', methods=['GET', 'POST'])   # 注册页面的视图
 def register():
     if request.method == 'GET':
         return render_template('register.html')
@@ -137,19 +85,18 @@ def register():
         username = request.form.get('username')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        message = validate(username, password1, password2)
+        message = validate(username, password1, password2)  # 利用函数检查合法性
         flash(message)
         if '成功' in message:
-            new_user = Users(username=username, password=generate_password_hash(password1))
-            # new_user = Users(username=username, password=password1)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('login'))
+            new_user = Users(username=username, password=generate_password_hash(password1)) # 新建用户
+            db.session.add(new_user)    # 添加到session队列
+            db.session.commit()     # 提交到数据库
+            return redirect(url_for('login'))   # 注册成功则跳转到登录界面
         else:
-            return render_template('register.html')
+            return render_template('register.html')     # 注册失败则回到注册界面
 
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])  # 登录页面的视图
 def login():
     if request.method == 'GET':
         return render_template('login.html')
@@ -158,85 +105,77 @@ def login():
         password = request.form.get('password1')
         message = validate(username, password)
         if '成功' in message:
-            session['username'] = username
+            session['username'] = username  # 登陆成功则把登录状态记录到session里
             session.permanent = True
-            return render_template('home.html', username=session['username'])
+            return render_template('home.html', username=session['username'])   # 登陆成功回到首页
         else:
             flash(message)
-            return render_template('login.html')
+            return render_template('login.html')    # 登录失败重新回到登录界面
 
 
-@app.route('/logout/')
+@app.route('/logout/')  # 注销的视图
 def logout():
-    session.clear()
-    return redirect(url_for('home'))
+    session.clear() # 注销则清楚session里的登录状态
+    return redirect(url_for('home'))    # 注销成功，回到首页
 
 
-# @app.context_processor
-# def my_context_processor():
-#     status = session.get('status', '')
-#     return {'status': status}
-
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])  # 上传页面的视图
 def upload():
-    if not session.permanent:
+    if not session.permanent:   # 检查登录状态
         flash("请先登录！")
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))   # 未登录则弹出未登录通知且跳转到登录界面
     else:
         if request.method == 'POST':
-            file = request.files['file']
-            base_path = os.path.abspath(os.path.dirname(__file__))
-            upload_path = os.path.join(base_path, '_upload', session['username'])
+            file = request.files['file']    # 获取文件上传请求
+            base_path = os.path.abspath(os.path.dirname(__file__))  # 本文件的绝对路径
+            upload_path = os.path.join(base_path, '_upload', session['username'])   # 上传的绝对路径
             print(upload_path)
-            if not os.path.exists(upload_path):
+            if not os.path.exists(upload_path):     # 检查上传路径是否存在，不存在则新建路径
                 os.makedirs(upload_path)
-            file.filename = file.filename.replace(' ', '__blank__')
+            file.filename = file.filename.replace(' ', '__blank__')     # 将文件名中的空格进行替换防止后续处理出错
             file_path = os.path.join(upload_path, file.filename)
-            file.save(file_path)
-            if request.form.get('file_description'):
+            file.save(file_path)    # 保存文件
+            if request.form.get('file_description'):    # 获取文件描述，若用户未填写文件描述则默认为文件名
                 file_description = request.form.get('file_description')
             else:
                 file_description = file.filename
             print(file_description)
-            file_type = file.filename.split(".")[1]
+            file_type = file.filename.split(".")[1]     # 根据文件名称获取文件扩展名
             print(file_type)
-            # file_type = os.path.splitext(file)[-1][1:]
-            mime_type = Mime_types.query.filter(Mime_types.name == file_type).first()
-            if not mime_type:
+            mime_type = Mime_types.query.filter(Mime_types.name == file_type).first()   # 根据文件扩展名在数据库中获取MIME类型
+            if not mime_type:   # 若数据库中MIME类型不包含上传文件的类型，则返回通知并删除已上传的文件，跳转到上传界面
                 flash('不支持的文件类型！')
                 os.remove(file_path)
                 return render_template('upload.html')
             else:
                 print(mime_type.template)
-                file_size = os.path.getsize(file_path)
-                user = Users.query.filter(Users.username == session['username']).first()
+                file_size = os.path.getsize(file_path)  # 获取文件大小
+                user = Users.query.filter(Users.username == session['username']).first()    # 获取当前用户的模型用于获取文件对应的用户ID
                 new_file = Files(filename=file.filename, file_description=file_description,
-                                 file_mime_type=mime_type.template, file_size=file_size, user_id=user.id)
-                check_new_file_name = Files.query.filter(Files.filename == new_file.filename).first()
+                                 file_mime_type=mime_type.template, file_size=file_size, user_id=user.id)   # 新建文件模型
+                check_new_file_name = Files.query.filter(Files.filename == new_file.filename).first()       # 查找是否有同名文件
                 check_new_file_user_id = Files.query.filter(Files.user_id == new_file.user_id).first()
                 print(check_new_file_user_id)
                 print(check_new_file_name)
-                if check_new_file_name is None or check_new_file_user_id is None:
+                if check_new_file_name is None or check_new_file_user_id is None:   # 判断同一用户是否上传过同名文件
                     check_new_file = True
                 else:
                     check_new_file = False
                 if check_new_file:
-                    # new_file.name_replace_blank()
-                    db.session.add(new_file)
+                    db.session.add(new_file)    # 将文件模型添加到session队列
                     db.session.commit()
                     print(check_new_file)
                     flash("文件上传成功！")
                 else:
                     flash('文件已存在！')
                     print(check_new_file)
-                    return render_template('upload.html')
-        return render_template('upload.html')
+                    return render_template('upload.html')   # 若文件已存在则回到上传界面
+        return render_template('upload.html')   # 上传成功，回到上传界面
 
 
-@app.route('/files', methods=['GET', 'POST'])
+@app.route('/files', methods=['GET', 'POST'])   # 我的文件页面视图
 def files():
-    if not session.permanent:
+    if not session.permanent:   # 检查登录状态
         flash("请先登录！")
         return redirect(url_for('login'))
     else:
@@ -295,7 +234,8 @@ def delete_file():
         filename = full_filename_list[-1]
         delete_file_user = Users.query.filter(Users.username == session['username']).first()
         print(delete_file_user.id)
-        will_delete_file = Files.query.filter(and_(Files.filename == filename), (Files.user_id == delete_file_user.id)).first()
+        will_delete_file = Files.query.filter(and_(Files.filename == filename),
+                                              (Files.user_id == delete_file_user.id)).first()
         print(will_delete_file.user_id)
         db.session.delete(will_delete_file)
         db.session.commit()
--- WK09homeworkZhouZhendong/flask_file_upload/config.py
+++ WK09homeworkZhouZhendong/flask_file_upload/config.py
@@ -2,13 +2,13 @@ DEBUG = True
 
 HOST = "127.0.0.1"
 PORT = "3306"
-DB = "flaskFileSystem"
-USER = "root"
-PASS = "linyheart"
+DB = "flaskFileSystem"  # 在MySQL数据库里建一个名称为这个的数据库，也可以修改名称
+USER = "root"   # MySQL数据库用户名
+PASS = "linyheart"  # 数据库密码
 CHARSET = "utf8"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset={}".format(USER, PASS, HOST, PORT, DB, CHARSET)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
-SECRET_KEY = "LINYHEART"
+SECRET_KEY = "LINYHEART"    # 保存文件使用的密码,应该可以随意修改
 
-UPLOAD_FOLDER = r'./_upload/'
+UPLOAD_FOLDER = r'./_upload/'   # 上传文件的路径，可以随意修改
